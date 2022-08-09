30b3ceeacb64c15502b57aa9b34a3487ced74c83
MahardhikaB <bredymahardhika@gmail.com>
2022-08-09 20:27:16 +0700

    first commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Button btnOk = findViewById(R.id.ok);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View v) {
+               btnOk.setBackgroundColor(Color.RED);
+           }
+        });
+    }
+}
